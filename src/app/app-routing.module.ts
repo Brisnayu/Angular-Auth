5e84da16c82487f763d4c020bb5e34d5646c3151
BrisnaPaezMon <brisnapaez25@gmail.com>
2024-07-14 13:20:44 +0200

    First commit develop branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
   { path: 'home', loadChildren: () => import('./features/home/home.module').then(mod => mod.HomeModule)},
   {
     path: 'login', loadChildren: () => import('./features/auth/auth.module').then(mod => mod.AuthModule)
